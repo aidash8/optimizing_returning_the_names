701035c06ce759e473aa583818adc15c353f73c3
aidash8 <shoydok@gmail.com>
2024-11-02 15:00:11 -0700

    Fixed the spaces after numbers in reading pdf

--- utils/read_human_generated_cards_2023.py.py
+++ utils/read_human_generated_cards_2023.py.py
@@ -17,17 +17,22 @@ def read_human_generated_cards_2023(dirname: str) -> List[Tuple[str]]:
             file_path = os.path.join(dirname, filename)
             reader = PdfReader(file_path)
             for i,page in enumerate(reader.pages):
-                text = page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False)
+                text = page.extract_text(extraction_mode="layout",layout_mode_space_vertically=False)
                 # print(repr(text))
                 if text:
                     lines = text.splitlines()
                     name = re.sub(r'\s+',' ',lines[0])
                     age = re.sub(r'\s+',' ',lines[1])
+                    # doesn't extract space after age (number)
+                    age = re.sub(r"(\d+)([А-Я]+)",r"\1 \2", age)
                     birth_info = re.sub(r'\s+',' ',lines[2])
                     occupation = re.sub(r'\s+',' ',lines[3])
                     if len(lines) > 6: # need to stitch
                         occupation += ' ' + re.sub(r'\s+',' ',lines[4])
                     death_info = re.sub(r'\s+',' ',lines[-2])
+                    # doesn't extract space after number
+                    death_info = re.sub(r"(\d+)([А-Я]+)",r"\1 \2", death_info)
+                    # print(repr(death_info))
                     id = lines[-1].strip()
                     if id.isnumeric(): # only get the cards with id
                         data.append((filename,i,id,name,age,birth_info,occupation,death_info,text))
